1e3374c6a51ba21b8c07a4a9cd8ac5defd46cdcd
Magí Tell Bonet <mtellb@gmail.com>
2019-03-30 19:16:02 +0100

    Revert "__main__.py arreglaoh"
    This reverts commit 3e79e867dc08627046dae3a435a8d947f08927a2.

--- Pract1/testzip/__main__.py
+++ Pract1/testzip/__main__.py
@@ -1,6 +1,7 @@
 import re
 from cos_backend import COSBackend
 import yaml
+import sys
 from collections import Counter
 import json
 
@@ -12,13 +13,22 @@ def main(args):
 	print(args)
 	s1 = json.dumps(args)
 	args = json.loads(s1)
+	fileName = args["fileName"]
+	bucket = args["bucket"]
+	rang = args["rang"]
 	counts = Counter()
-	fileFromServer = odb.get_object(args["bucket"], args["fileName"], extra_get_args={'Range':args["rang"]}).decode('UTF-8')
+	fileFromServer = odb.get_object(bucket, fileName, extra_get_args={'Range':rang}).decode('UTF-8')
 
-	#stringFiltered = re.sub('[^A-Za-z0-9 \n]+', '', fileFromServer)
+	#newFile = open("newBible.txt", "w")
+	stringFiltered = re.sub('[^A-Za-z0-9 \n]+', '', fileFromServer)
 	stringFiltered = re.sub('[^A-Za-z \n]+', '', fileFromServer)
 	stringSplitted = re.split("\ |\n", stringFiltered)
 	stringSplitted.remove("")
 
 	counts.update(word.strip('.,?!"\'').lower() for word in stringSplitted)
+	print(dict(counts))
 	return (dict(counts))
+# if __name__ == "__main__":
+# 	print(main())
+#fileFromServer = odb.get_object("magisd", "bible.txt", extra_get_args={'Range':'bytes=0-100'}).decode('UTF-8')
+
