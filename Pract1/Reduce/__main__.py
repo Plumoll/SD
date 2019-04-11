e0cfd1cbd4d363fc3bf235c172e8e260567776ab
Magí Tell Bonet <mtellb@gmail.com>
2019-04-11 21:25:21 +0200

    orchestrator and reduce commented

--- Pract1/Reduce/__main__.py
+++ Pract1/Reduce/__main__.py
@@ -1,70 +1,109 @@
 import pika, json, re
+#! collections.abc?!?!?
 from collections import Counter
 from cos_backend import COSBackend
 
+#dictionary used to save the result between callbacks
 df = {}
+#number of iterations done
 n = 0
+#number max of iterations
 iterations = 0
+#obd needed for the use of ibm COS
 odb = None
+#the name of the book we are analysing, needed for the result file's name
+fileName = ""
 
+#CountingWords function send its result as a body with this format: {'words': wordsNumber}
 def CountingWordsCallback(ch, method, properties, body):
     global n
     global df
-    global odb
     global iterations
 
-    received_data = json.loads(body)
+    #the body given is converted into a json
+    received_data = json.loads(body.decode('UTF-8'))
+    #create auxiliar dictionary
+    daux = {}
+    #daux = df + received data
     daux = Counter(df) + Counter(received_data)
+    #counter -> dict
     df = dict(daux)
     n += 1
+    #!for me
     print(n)
     print(df)
+
     if(n == iterations):
+        #? can we put this into a function?
+        global odb
+        global fileName
+        #dict -> json
         dumped_json_string = json.dumps(df)
-        odb.put_object("magisd", 'ResultCountingWord.txt', dumped_json_string)
+        #save result to cloud  name:book.txt -> bookCountingWordResult.txt
+        odb.put_object("magisd", fileName[:-4] + 'CountingWordResult.txt', dumped_json_string)
+        #once we've saved the result the connection will be closed
         ch.stop_consuming()
 
+#WordCount function saves the result in the COS and sends the file's name as body
 def WordCountCallback(ch, method, properties, body):
         global df
         global n
         global odb
         global iterations
 
+        #fileFromServer -> wordCount result. Got it from download the file its name is the body given
         fileFromServer = odb.get_object("magisd", body.decode('UTF-8')).decode('UTF-8', errors='ignore')
+        #Delete the temporal file created by wordCount function
         odb.delete_object("magisd", body.decode('UTF-8', errors='ignore'))
+        #String -> json
         received_data = json.loads(fileFromServer)
 
+        #create auxiliar dictionary
         daux = {}
+        #daux = df + received_data
         daux = Counter(df) + Counter(received_data)
+        #counter -> dict
         df = dict(daux)
         n += 1
+        #!for me
         print(n)
         if(n == iterations):
-            stringFiltered = re.sub('[^A-Za-z \n]+', '', body.decode('UTF-8'))
+            #? can we put this into a function?
+            global fileName
+            #dictionary -> json
             dumped_json_string = json.dumps(df)
-            odb.put_object("magisd", stringFiltered[:-3]+'ResultWordCount.txt', dumped_json_string)
+            #save result to cloud  name:book.txt -> bookWordCountResult.txt
+            odb.put_object("magisd", fileName[:-4] + 'WordCountResult.txt', dumped_json_string)
+            #once we've saved the result the connection will be closed
             ch.stop_consuming()
             
 #def main(args):
 def main(args):
     global odb
     global iterations
+    global fileName
     
+    #load necessary arguments
     s1 = json.dumps(args)
     args = json.loads(s1)
-    odb = COSBackend(args["endpoint"], args['secret_key'], args['access_key'])
+    odb = COSBackend(args["endpoint"], args["secret_key"], args["access_key"])
     url = args["url"]
-    iterations=10
+    iterations = args["iter"]
+    fileName = args["fileName"]
 
+    #Pika configuration
     params = pika.URLParameters(url)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    channel.queue_declare(queue='WordCount')
-    channel.queue_declare(queue='CountingWords')
+    #a queue for each function
+    channel.queue_declare(queue="WordCount")
+    channel.queue_declare(queue="CountingWords")
+    #each queue has its own callback
     channel.basic_consume(WordCountCallback, queue="WordCount", no_ack=True)
     channel.basic_consume(CountingWordsCallback, queue="CountingWords", no_ack=True)
-    
+    #start receiving messages
     channel.start_consuming()
+    #close rabbitmq's connection
     connection.close()
     return {}
 
--- Pract1/orchestrator.py
+++ Pract1/orchestrator.py
@@ -2,42 +2,83 @@ from ibm_cf_connector import CloudFunctions
 import sys, yaml, time
 from cos_backend import COSBackend
 
-def selectRange(fileFromServer, rang):
+
+#this function calcules a topRange which doesn't cut any word
+#   comments:
+#       it starts searching from the end and when it finds an space returns the value
+#       the range of data downloaded for this is 20 since the average words lenth is more or less 5 so it will have an space always
+#   param:
+#       fileName    ->  name of the file
+#       rang        ->  rang from which we want to analyse
+#       res         ->  loaded configuration from ibm_cloud_config
+def selectRange(fileName, rang, res):
+    #configure COS library
+    odb = COSBackend(res['ibm_cos'])
+    fileFromServer = odb.get_object(res['ibm_cos']["bucket"], fileName, extra_get_args={'Range':'bytes={0}-{1}'.format(rang-20, rang)}).decode('UTF-8',errors='ignore')
     while(fileFromServer[-1] != " "):
         fileFromServer = fileFromServer[:-1]
         rang = rang - 1
     return rang
 
+#this function invokes the functionType given as an argument n times to IBM Cloud
+#   param: 
+#       function    -> functionType
+#       nfunctions  -> times we want to invoke the function (CountingWords - WordCount)
+#                                                           (reduce will only be invoked once)
+#       fileSize    -> total size of the file we want to analyse
+#       res         -> loaded configuration from ibm_cloud_config
+def invokeFunctions(function, nFunctions, fileSize, fileName, res):
+    #last invoked function's topRange
+    bottomRange = 0
+    #configure cloud function library
+    cf = CloudFunctions(res['ibm_cf'])
+    #loop invoking functions
+    #range(0, nFunctions-1) since the last one needs to be invoked different
 
-def invokeFunctions(function, nFunctions, fileSize, fileName, res, cf):
-    bottomRang = 0
     for i in range(0, nFunctions-1):
-        topRang = int(fileSize/nFunctions) + bottomRang
-        fileFromServer = odb.get_object("magisd", fileName, extra_get_args={'Range':'bytes={0}-{1}'.format(topRang-20, topRang)}).decode('UTF-8',errors='ignore')
-        topRang = selectRange(fileFromServer, topRang)
-        cf.invoke(function ,{"bucket": res['ibm_cos']["bucket"], "fileName": fileName, "rang": "bytes={0}-{1}".format(bottomRang, topRang), "endpoint":res['ibm_cos']["endpoint"],
-                             "access_key":res['ibm_cos']["access_key"], "secret_key":res['ibm_cos']["secret_key"], "url":res["rabbitmq"]["url"],
-                             "functionNumber":str(i)})
-        bottomRang = topRang
+        #fileSize/nFunctions as every function will operate with more or less the same amount of data
+        #bottomRange to not give the same data as the previous one
+        topRange = int(fileSize/nFunctions) + bottomRange
+        #avoid any cut words
+        topRange = selectRange(fileName, topRange, res)
+        #invoke the function with all its parameters
+        #param -> res, filemame, bottom and top range, fileNumber
+        cf.invoke(function ,{"res": res, "fileName": fileName, "rang": "bytes={0}-{1}".format(bottomRange, topRange), "functionNumber":str(i)})
+        #update the bottomRange
+        bottomRange = topRange
+    
+    #same as the previous ones but topRange is replaced for fileSize
+    cf.invoke(function ,{"res": res, "fileName": fileName, "rang": "bytes={0}-{1}".format(bottomRange, fileSize), "functionNumber":str(i)})
 
-    cf.invoke(function ,{"bucket": res['ibm_cos']["bucket"], "fileName": fileName, "rang": "bytes={0}-{1}".format(bottomRang, fileSize), "endpoint":res['ibm_cos']["endpoint"],
-                            "access_key":res['ibm_cos']["access_key"], "secret_key":res['ibm_cos']["secret_key"], "url":res["rabbitmq"]["url"],
-                             "functionNumber":str(nFunctions)})
-
-    _ = cf.invoke_with_result("reduce" ,{"bucket": res['ibm_cos']["bucket"], "endpoint":res['ibm_cos']["endpoint"],
-                            "access_key":res['ibm_cos']["access_key"], "secret_key":res['ibm_cos']["secret_key"], "url":res["rabbitmq"]["url"],
-                             "iter":nFunctions})
+    #invoke the reduce function(recieves messages from the other ones)
+    #invoke with result is necessary here as we want the total needed to finish.
+    #param -> res, nIterations, fileName
+    _ = cf.invoke_with_result("reduce" ,{"res": res, "iter":nFunctions, "fileName":fileName})
     
 if __name__=='__main__':
-    fileName=sys.argv[1]
-    nFunctions=int(sys.argv[2])
+    #read the arguments given
+    fileName = sys.argv[1]
+    nFunctions = int(sys.argv[2])
+    functionType = sys.argv[3]
+    #load config file
     with open('ibm_cloud_config', 'r') as config_file:
         res = yaml.safe_load(config_file)
-    cf = CloudFunctions(res['ibm_cf'])
+    #configure COS library
     odb = COSBackend(res['ibm_cos'])
-    fileSize = int(odb.head_object("magisd", fileName)["content-length"])
-
+    #chech the total file of the file given as argument
+    fileSize = int(odb.head_object(res['ibm_cos']["bucket"], fileName)["content-length"])
+    #start clock
     start = time.time()
-    invokeFunctions(sys.argv[3], nFunctions,  fileSize, fileName, res, cf)
+    #call the function nFunctions times and the reduce at the end
+    invokeFunctions(functionType, nFunctions,  fileSize, fileName, res)
+    #finish clock
     end = time.time()
     print(end-start)  
+
+    #! for me
+    #  cf.invoke(function ,{"bucket": res['ibm_cos']["bucket"], "fileName": fileName, "rang": "bytes={0}-{1}".format(bottomRange, fileSize), "endpoint":res['ibm_cos']["endpoint"],
+    #                         "access_key":res['ibm_cos']["access_key"], "secret_key":res['ibm_cos']["secret_key"], "url":res["rabbitmq"]["url"],
+    #                          "functionNumber":str(nFunctions)})
+# _ = cf.invoke_with_result("reduce" ,{"bucket": res['ibm_cos']["bucket"], "endpoint":res['ibm_cos']["endpoint"],
+#                             "access_key":res['ibm_cos']["access_key"], "secret_key":res['ibm_cos']["secret_key"], "url":res["rabbitmq"]["url"],
+#                              "iter":nFunctions, "fileName":fileName})
